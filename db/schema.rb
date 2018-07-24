9dd8c5681f2000333fc19826be6b8df3ba1e30d2
Gabriel Van de Sande <gabevandesande@gmail.com>
2018-07-24 19:18:41 +0200

    final:

--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,11 @@ ActiveRecord::Schema.define(version: 2018_05_30_223438) do
   enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "text"
+    t.string "first"
+    t.string "last"
+    t.string "company"
+    t.string "website"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
